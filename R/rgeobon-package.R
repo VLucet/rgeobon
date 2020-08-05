3fd8c96f97e66aa7513bb96f5332f0dbd32ebdc8
VLucet <valentin.lucet@gmail.com>
2020-08-05 10:48:26 -0400

    updates doc

---
+++ R/rgeobon-package.R
@@ -0,0 +1,9 @@
+#' rgeobon: the R interface to GEOBON's EBV open data portal API
+#'
+#' The goal of rgeobon is to provide an interface between R and the GEOBON open
+#' data portal RESTfull API (please note that the portal is currently in beta).
+#' It allows to consult the datasets available on the portal and download them.
+#'
+#' @docType package
+#' @name rgeobon
+NULL
